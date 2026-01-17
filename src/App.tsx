553e16232a1cd66e2154c6b38c36f70d06a8d811
“graciegould” <graciegould5@gmail.com>
2026-01-17 15:30:59 -0800

    shrink the image for the demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,19 @@ import { AutoPlay, DynamicInterpolation } from './demos';
 function App() {
   return (
     <div className="App">
-      {/*
-        AutoPlay Demo - plays animation once automatically
-
-        <AutoPlay
-          gridSize={10}
-          shape="triangle"
-          duration={4}
-          startOpacity={0.1}
-          endOpacity={1}
-          targetPosition="center"
-          imageWidth="80%"
-        />
-      */}
-
+      {/* <AutoPlay
+        gridSize={10}
+        shape="square"
+        duration={4}
+        startOpacity={0.1}
+        endOpacity={1}
+        targetPosition="center"
+        imageWidth={"40%"}
+      /> */}
       {/*
         DynamicInterpolation Demo - control animation with sliders
       */}
+      
       <DynamicInterpolation
         gridSize={10}
         initialShape="square"
@@ -29,7 +25,7 @@ function App() {
         initialEndOpacity={1}
         initialPositionX={50}
         initialPositionY={50}
-        initialScale={80}
+        initialScale={50}
       />
     </div>
   );
